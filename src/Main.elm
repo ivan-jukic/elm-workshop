1c9e404b47f0b95f8005c5e0a1c07f89f5695885
Ivan <ivan.jukic14@gmail.com>
2020-01-26 18:38:16 +0000

    Added some styling to the app

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,8 @@
 module Main exposing (..)
 
 import Browser exposing (sandbox)
-import Html exposing (Html, button, div, input, text)
-import Html.Attributes exposing (type_, value)
+import Html exposing (Html, button, div, h4, input, text)
+import Html.Attributes exposing (style, type_, value)
 import Html.Events exposing (onClick, onInput)
 
 
@@ -68,27 +68,58 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ div []
+    div
+        [ style "width" "400px"
+        , style "margin" "20px auto"
+        , style "padding" "20px"
+        , style "background-color" "#F2F2F2"
+        , style "border-radius" "4px"
+        ]
+        [ h4
+            [ style "margin" "0 0 20px"
+            ]
+            [ text "ToDo App Workshop"
+            ]
+        , div
+            [ style "margin-bottom" "20px"
+            ]
             (model.todoItems
                 |> List.reverse
                 |> List.map
                     (\todoItem ->
-                        div []
+                        div
+                            [ style "display" "flex"
+                            , style "padding" "6px 0"
+                            ]
                             [ text todoItem
-                            , button [ onClick (RemoveTodoItem todoItem) ] [ text "×" ]
+                            , button
+                                [ onClick (RemoveTodoItem todoItem)
+                                , style "padding" "0"
+                                , style "width" "18px"
+                                , style "height" "18px"
+                                , style "color" "white"
+                                , style "border" "none"
+                                , style "margin-left" "auto"
+                                , style "border-radius" "50px"
+                                , style "background-color" "#999"
+                                ]
+                                [ text "×" ]
                             ]
                     )
             )
-        , div []
+        , div
+            [ style "display" "flex"
+            ]
             [ input
                 [ type_ "text"
                 , value model.newTodoItem
                 , onInput SetNewTodoItemName
+                , style "flex" "1"
                 ]
                 []
             , button
                 [ onClick AddTodoItem
+                , style "cursor" "pointer"
                 ]
                 [ text "Add +" ]
             ]
